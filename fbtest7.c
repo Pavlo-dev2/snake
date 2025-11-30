aedd2513c18e940f71061a7e560d7e626c3b561b
Pavlo <pavlokudil.dev@gmail.com>
2025-11-30 17:56:17 +0100

    Working output version
    drawrect and clearfeld working rigth

--- fbtest7.c
+++ fbtest7.c
@@ -8,12 +8,10 @@
 #include <stdint.h>
 
 int drawrect(unsigned char r, unsigned char g, unsigned char b, char x, char y, int un, int fb0);
+int clearfeld(unsigned char r, unsigned char g, unsigned char b, int fb0);
 int* info(int fb0);
 
 
-//info struckts
-static struct fb_var_screeninfo vinfo;
-static struct fb_fix_screeninfo finfo;
 
 int main() {
     int fb = open("/dev/fb0", O_RDWR);
@@ -32,27 +30,32 @@ int main() {
     drawrect(255, 255, 0, 0, 1, a, fb);
     drawrect(255, 0, 255, 1, 1, a, fb);
     drawrect(0, 255, 255, 2, 1, a, fb);
-    drawrect(255, 255, 255, 0, 2, a, fb);
+    drawrect(128, 128, 128, 0, 2, a, fb);
     drawrect(0, 0, 0, 1, 2, a, fb);
     drawrect(128, 128, 128, 2, 2, a, fb);*/
 
+    clearfeld(0, 255, 0, fb);
     drawrect(0, 0, 255, 0, 0, a, fb);
-    drawrect(0, 0, 255, 15, 0, a, fb);
-    drawrect(0, 0, 255, 15, 15, a, fb);
-    drawrect(0, 0, 255, 0, 15, a, fb);
+    drawrect(255, 0, 0, 15, 0, a, fb);
+    drawrect(255, 255, 255, 15, 15, a, fb);
+    drawrect(128, 128, 128, 0, 15, a, fb);
     //drawrect(0, 255, 0, 1, 0, a, fb);
     //drawrect(255, 255, 0, 0, 1, a, fb);
     //drawrect(255, 0, 0, 1, 1, a, fb);
 
+    sleep(3);
     close(fb);
     return 0;
 }
 
 
-//funktion to draw a square
-int drawrect(unsigned char r, unsigned char g, unsigned char b, char x, char y, int un, int fb0)
+//clear feld in the midle of the screan, size y_max*y_max
+int clearfeld(unsigned char r, unsigned char g,  unsigned char b, int fb0)
 {
-
+	//info struckts
+	struct fb_var_screeninfo vinfo;
+	struct fb_fix_screeninfo finfo;
+	
 	//get infos
 	if (ioctl(fb0, FBIOGET_VSCREENINFO, &vinfo) == -1 || ioctl(fb0, FBIOGET_FSCREENINFO, &finfo) == -1)
 	{
@@ -71,9 +74,94 @@ int drawrect(unsigned char r, unsigned char g, unsigned char b, char x, char y, 
 		return 1;
 	}
 
-	long xdif, ydif;
-	xdif = (vinfo.xres - ((float)((x+1.0)/un)*vinfo.yres))/2;//diferense x
-	ydif = (vinfo.yres - ((float)(y+1)/un*vinfo.yres))/2;//diferense y
+	//feld cords, xstart, xend, ystart, yend, in the midle of the screan;
+	long feldcords[4] = {0};
+	feldcords[0] = (vinfo.xres - (float)vinfo.yres)/2.0;
+	feldcords[1] = feldcords[0] + vinfo.yres;
+	feldcords[2] = (vinfo.yres - (float)vinfo.yres)/2.0;
+	feldcords[3] = feldcords[2] + vinfo.yres;
+	
+	//printf("XS0: %lu\nXE1: %lu\n", feldcords[0], feldcords[1]);
+	//printf("YS2: %lu\nYE3: %lu\n", feldcords[2], feldcords[3]);
+
+
+	//feld size in pixels
+	float feld_heigth, feld_wigth;
+	feld_wigth = feldcords[1] - feldcords[0];//x length
+	feld_heigth = feldcords[3] - feldcords[2];//y length
+
+	//printf("feld_wigth: %f;\nfeld_heigth: %f;\n", feld_wigth, feld_heigth);
+	
+	//sleep(1);
+
+	//draw feld
+	for (long ay = feldcords[2]; ay < feldcords[3]; ay++)
+	{
+		for (long ax = feldcords[0]; ax < feldcords[1]; ax++)
+		{
+            		long location = (ax + vinfo.xoffset) * (vinfo.bits_per_pixel / 8) + (ay + vinfo.yoffset) * finfo.line_length;
+			
+			if (vinfo.bits_per_pixel == 32)
+			{
+				fbp[location] = b;
+				fbp[location+1] = g;
+				fbp[location+2] = r;
+				fbp[location+3] = 0;
+			}
+		
+		}
+	}
+
+	//end
+	munmap(fbp, screensize);
+}
+
+//print fb0 info
+int* info(int fb0)
+{
+	//info struckts
+	struct fb_var_screeninfo vinfo;
+	struct fb_fix_screeninfo finfo;
+	
+	//get infos
+	if (ioctl(fb0, FBIOGET_VSCREENINFO, &vinfo) == -1 || ioctl(fb0, FBIOGET_FSCREENINFO, &finfo) == -1)
+	{
+		perror("Error while opening fb0");
+		close(fb0);
+		return NULL;
+	}
+	
+	printf("vinfo.xres: %f\nvinfo.yres: %f\n", (float) vinfo.xres, (float) vinfo.yres);
+	printf("finfo.smem_len: %f\n", (float) finfo.smem_len);
+	printf("vinfo.xoffset: %f\nvinfo.yoffset: %f\n", (float) vinfo.xoffset, (float) vinfo.yoffset);
+	printf("vinfo.xbits_per_pixel: %f\nvinfo.byts_per_pixel: %f\n", (float) vinfo.bits_per_pixel, (float) vinfo.bits_per_pixel/8);
+	printf("finfo.line_length: %f\n\n", (float) finfo.line_length);
+}
+
+//funktion to draw a square
+int drawrect(unsigned char r, unsigned char g, unsigned char b, char x, char y, int un, int fb0)
+{
+	//info struckts
+	struct fb_var_screeninfo vinfo;
+	struct fb_fix_screeninfo finfo;
+	
+	//get infos
+	if (ioctl(fb0, FBIOGET_VSCREENINFO, &vinfo) == -1 || ioctl(fb0, FBIOGET_FSCREENINFO, &finfo) == -1)
+	{
+		perror("Error while opening fb0");
+		close(fb0);
+		return 1;
+	}
+
+	//make fb0 a pointer
+	long screensize = finfo.smem_len;
+	uint8_t *fbp = (uint8_t*)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb0, 0);
+	if (fbp == MAP_FAILED)
+	{
+		perror("Mmap failed");
+		close(fb0);
+		return 1;
+	}
 	
 	//feld cords, xstart, xend, ystart, yend, in the midle of the screan;
 	long feldcords[4] = {0};
@@ -102,23 +190,6 @@ int drawrect(unsigned char r, unsigned char g, unsigned char b, char x, char y, 
 
 	//sleep(1);
 
-	/*for (float ay = (float)y/un * vinfo.yres + ydif; ay < (float)(y+1)/un*vinfo.yres + ydif; ay++)
-	{
-		for (float ax = ((float)x/un) * vinfo.yres + xdif; ax < (float)(x+1)/un * vinfo.yres + xdif; ax++)
-		{
-            		long location = (ax + vinfo.xoffset) * (vinfo.bits_per_pixel / 8)
-			+ (ay + vinfo.yoffset) * finfo.line_length;
-			
-			if (vinfo.bits_per_pixel == 32)
-			{
-				fbp[location] = b;
-				fbp[location+1] = g;
-				fbp[location+2] = r;
-				fbp[location+3] = 0;
-			}
-		}
-	}*/
-
 	long xst = feldcords[0] + block_wigth*x;//x start
 	long xed = xst+block_wigth;//x end
 	long yst = feldcords[2] + block_heigth*y;//y start
@@ -144,21 +215,4 @@ int drawrect(unsigned char r, unsigned char g, unsigned char b, char x, char y, 
 
 	//end
 	munmap(fbp, screensize);
-}
-
-int* info(int fb0)
-{
-	//get infos
-	if (ioctl(fb0, FBIOGET_VSCREENINFO, &vinfo) == -1 || ioctl(fb0, FBIOGET_FSCREENINFO, &finfo) == -1)
-	{
-		perror("Error while opening fb0");
-		close(fb0);
-		return NULL;
-	}
-	
-	printf("vinfo.xres: %f\nvinfo.yres: %f\n", (float) vinfo.xres, (float) vinfo.yres);
-	printf("finfo.smem_len: %f\n", (float) finfo.smem_len);
-	printf("vinfo.xoffset: %f\nvinfo.yoffset: %f\n", (float) vinfo.xoffset, (float) vinfo.yoffset);
-	printf("vinfo.xbits_per_pixel: %f\nvinfo.byts_per_pixel: %f\n", (float) vinfo.bits_per_pixel, (float) vinfo.bits_per_pixel/8);
-	printf("finfo.line_length: %f\n\n", (float) finfo.line_length);
 }
